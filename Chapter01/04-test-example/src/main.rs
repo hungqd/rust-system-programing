79389260b00de87ce421245a3d5886adf34f9e1f
Đặng Quang Hưng <hungdq58@gmail.com>
2021-07-22 17:55:27 +0700

    Create tests module

--- Chapter01/04-test-example/src/main.rs
+++ Chapter01/04-test-example/src/main.rs
@@ -8,7 +8,11 @@ fn get_process_id() -> u32 {
     process::id()
 }
 
-#[test]
-fn test_if_process_id_is_returned() {
-    assert_ne!(get_process_id(), 0, "There is error in code");
+#[cfg(test)]
+mod tests {
+    use super::get_process_id;
+    #[test]
+    fn test_if_process_id_is_returned() {
+        assert_ne!(get_process_id(), 0, "There is error in code");
+    }
 }
